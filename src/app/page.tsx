9a46583dcd87bbaf57eb878038741cc799dd366d
Vladislav Glumov <19gvi09@gmail.com>
2023-11-11 23:53:51 +0300

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <video src={"/video.mp4"} width="100%" controls="controls" autoPlay={true} />
+      <video src={"/video.mp4"} width="100%" controls autoPlay={true} />
     </main>
   )
 }
